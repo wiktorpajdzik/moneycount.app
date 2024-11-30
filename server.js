9eea23a8375d792edac3193b25df00133f7e2394
Wiktor Pajdzik <114699119+wiktorpajdzik@users.noreply.github.com>
2024-11-30 16:11:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const crypto = require('crypto');
 // Używanie cookie-parser i CORS
 app.use(cookieParser());
 app.use(cors({
-  origin: '*',
+  origin: "*",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
